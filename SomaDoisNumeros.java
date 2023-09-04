d4f2f49a7c06eb12ee1ad0959d1bc22839a42084
Gabriel Bello <gassis574@gmail.com>
2023-09-04 19:38:21 -0300

    ap02(input-output): declaracao de variaveis

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SomaDoisNumeros{
+    public static void main (String [] args){
+     
+        Scanner x, y = new Scanner(System.in);
+        double primeiroOperando;
+        double segundoOperando;
+        double resultado;
+
+        // Scanner x = new Scanner (" digita ai ");
+        // Scanner y = new Scanner (" digita ai denovo ");
+
+        
+    }
+
+
+}
