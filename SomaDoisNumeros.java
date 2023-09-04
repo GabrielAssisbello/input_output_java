b88840ffab5ad65d79f051618630246e5c217800
Gabriel Bello <gassis574@gmail.com>
2023-09-04 20:41:59 -0300

    ap02(input-output): resultado ok

--- SomaDoisNumeros.java
+++ SomaDoisNumeros.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 public class SomaDoisNumeros{
     public static void main (String [] args){
      
-        Scanner leitor = new Scanner(System.in);
-        double primeiroOperando;
-        double segundoOperando;
-        double resultado;
+        try (Scanner leitor = new Scanner(System.in)) {
+            double primeiroOperando;
+            double segundoOperando;
+            double resultado;
 
-        System.out.println(" digita ai ");
-        primeiroOperando = leitor.nextDouble();
-        
-        System.out.println(" digita ai ");
-        segundoOperando = leitor.nextDouble();
-        
-        resultado = primeiroOperando + segundoOperando;
+            System.out.println(" digita ai ");
+            primeiroOperando = leitor.nextDouble();
+            
+            System.out.println(" digita ai denovo ");
+            segundoOperando = leitor.nextDouble();
+            
+            resultado = primeiroOperando + segundoOperando;
+
+            System.out.print(resultado);
+        }
         
 
 
         
     }
-
-
+    
 }
